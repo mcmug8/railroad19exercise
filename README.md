cfe8dcbcd0ad85039740230f855b0d023cadf208
Tim Ng <timn8@hotmail.com>
2019-02-21 18:14:49 -0800

    first commit
    first commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# railroad19exercise
-test
---
+++ railroad19exericse/src/main/java/railroad19exericse/railroad19exericse/App.java
@@ -0,0 +1,13 @@
+package railroad19exericse.railroad19exericse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
